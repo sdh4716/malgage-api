02871edf50a7cefb3240294ff98248bf57b3c371
dahyun <sdh4716@naver.com>
2025-07-17 00:26:29 +0900

    init: 프로젝트 초기 세팅 - gitignore, yml 설정, 디렉토리 구조 구성

---
+++ src/main/java/com/darong/malgage_api/MalgageApiApplication.java
@@ -0,0 +1,13 @@
+package com.darong.malgage_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalgageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalgageApiApplication.class, args);
+	}
+
+}
